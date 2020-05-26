979262aef9de7185330406af23985bc8d4a22234
Norman Breau <norman@normanbreau.com>
2020-05-25 22:21:47 -0300

    Added comments to IDefinition

--- src/IDefinition.ts
+++ src/IDefinition.ts
@@ -1,5 +1,16 @@
 
 export interface IDefinition {
+    /**
+     * The path to the file
+     * @required
+     */
     path: string;
+
+    /**
+     * The mime type. If not set, common set will be used based on mime type.
+     * If extension is not recognizable, `application/octet-stream` will be used as
+     * a last resort.
+     * @optional
+     */
     type?: string;
 }
